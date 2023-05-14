3cd53e69a962e801746671886806200b7d93525d
Chera <chera1904@mail.ru>
2023-05-14 16:36:11 +0600

    Added django-filter library; Filter classes for views added; Used black.

--- main/test/test_admin.py
+++ main/test/test_admin.py
@@ -14,13 +14,15 @@ class TestAdmin(APITestCase):
     @classmethod
     def setUpTestData(cls) -> None:
         super().setUpTestData()
-        cls.admin = User.objects.create_superuser("test@test.ru", email=None, password=None)
+        cls.admin = User.objects.create_superuser(
+            "test@test.ru", email=None, password=None
+        )
         cls.client = APIClient()
         cls.client.force_login(cls.admin)
 
     @classmethod
     def assert_forms(
-            cls, model: Type[models.Model], key: int, check_actions: Container = ()
+        cls, model: Type[models.Model], key: int, check_actions: Container = ()
     ) -> None:
         app_label = model._meta.app_label
         model_name = model._meta.model_name
