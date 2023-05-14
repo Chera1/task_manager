bae434c4330b2821e563912590e628ad506ff95b
Chera <chera1904@mail.ru>
2023-05-14 14:39:38 +0600

    Added ViewSets for models.

--- main/views.py
+++ main/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .serializers import UserSerializer, TaskSerializer, TagSerializer
+from .models import User, Task, Tag
+from rest_framework import viewsets
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.order_by("id")
+    serializer_class = UserSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all().select_related('performer', 'author').prefetch_related('tags')
+    serializer_class = TaskSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
