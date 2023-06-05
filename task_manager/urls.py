3c1a3892a43954361113dcb8638160f09c4c9165
Chera <chera1904@mail.ru>
2023-06-05 10:25:10 +0600

    Deleted comment lines

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -27,11 +27,13 @@ schema_view = get_schema_view(
         default_version="v1",
         description="Description of the API",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="CherednichenkoArtemAlbertovich@gmail.com", name="Artem"),
-        license=openapi.License(name="BSD License")
+        contact=openapi.Contact(
+            email="CherednichenkoArtemAlbertovich@gmail.com", name="Artem"
+        ),
+        license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=[permissions.AllowAny]
+    permission_classes=[permissions.AllowAny],
 )
 
 router = routers.SimpleRouter()
@@ -45,7 +47,17 @@ urlpatterns = [
     ),  # Для подключения модуля авторизации (кнопка login)
     path("api/", include(router.urls)),
     path("admin/", task_manager_admin_site.urls),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 ]
