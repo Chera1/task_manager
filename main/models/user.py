18e9a259bca93d52f751472530461ac70a45f504
Chera <chera1904@mail.ru>
2023-06-06 16:35:34 +0600

    Added phone and date of birth at User model

--- main/models/user.py
+++ main/models/user.py
@@ -13,3 +13,5 @@ class User(AbstractUser):
     role = models.CharField(
         max_length=255, default=TypeRole.developer, choices=TypeRole.choices
     )
+    date_of_birth = models.DateField(null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
