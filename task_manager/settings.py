569864a8243c55373b6ad6deb0767fa94862285d
Chera <chera1904@mail.ru>
2023-06-12 11:09:43 +0600

    Email notification feature added.

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -168,9 +168,9 @@ def strtobool(val: str):
     are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
     'val' is anything else."""
     val = val.lower()
-    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+    if val in ("y", "yes", "t", "true", "on", "1"):
         return 1
-    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+    elif val in ("n", "no", "f", "false", "off", "0"):
         return 0
     else:
         raise ValueError("invalid truth value %r" % (val,))
